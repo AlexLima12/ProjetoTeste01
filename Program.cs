39b153af5790a5fadd5832bbc6d81c69b5964019
FIC <alexplima@yahoo.com.br>
2017-10-05 21:03:43 -0300

    Copiar arquivos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           FazerCafe("caramelo");
+           
+            
+        }
+
+    static void FazerCafe(string capsula)
+    {
+        System.Console.WriteLine("Esquentar água");
+        System.Console.WriteLine("colocar o pó de café");
+        System.Console.WriteLine("Colocar " + capsula);
+    }
+
+
+
+    }
+}
